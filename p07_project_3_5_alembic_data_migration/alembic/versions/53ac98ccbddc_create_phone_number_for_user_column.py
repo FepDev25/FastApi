aca6365a43defc2eace5f4db6669b489af908a45
Felipe <lamediatricolor@gmail.com>
2025-12-26 18:54:32 -0500

    Proyecto 4: Agregando codigo para downgrade

--- p07_project_3_5_alembic_data_migration/alembic/versions/53ac98ccbddc_create_phone_number_for_user_column.py
+++ p07_project_3_5_alembic_data_migration/alembic/versions/53ac98ccbddc_create_phone_number_for_user_column.py
@@ -19,10 +19,8 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    """Upgrade schema."""
     op.add_column('users', sa.Column('phone_number', sa.String(length=15), nullable=True))
 
 
 def downgrade() -> None:
-    """Downgrade schema."""
-    pass
+    op.drop_column('users', 'phone_number')
